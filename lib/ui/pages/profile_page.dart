d67df59adc67c6aeffb4d60adda2aec025c920fa
Andi Dwi Prastyo <2141720046@student.polinema.ac.id>
2023-10-29 20:16:15 +0700

    feat:Fix Profile Page

--- lib/ui/pages/profile_page.dart
+++ lib/ui/pages/profile_page.dart
@@ -1,13 +1,115 @@
 import 'package:flutter/material.dart';
+import 'about_page.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Profile Page!'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Akun Saya',
+            style: TextStyle(fontWeight: FontWeight.bold)),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 20),
+            const Row(
+              mainAxisAlignment:
+                  MainAxisAlignment.start, // Align everything to the left
+              children: [
+                CircleAvatar(
+                  radius: 40, // Adjust the size as needed
+                  backgroundImage: AssetImage('Assets/images/profile.png'),
+                ),
+                SizedBox(
+                  width: 20,
+                ), // Add spacing between the CircleAvatar and Text
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Vladimir Putin',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.left),
+                    SizedBox(height: 5),
+                    Text('vladimirputin@gmail.com'),
+                    SizedBox(height: 5),
+                    Text('083848492377')
+                  ],
+                )
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 20),
+                const Text('Pengaturan',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.left),
+                const SizedBox(height: 20),
+                TextButton(
+                  onPressed: () {
+                    // Navigate to the Edit Profile page
+                  },
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(Icons.edit),
+                      SizedBox(width: 10),
+                      Text('Edit Profil', style: TextStyle(fontSize: 18)),
+                      Spacer(),
+                      Icon(Icons.arrow_forward_ios),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const AboutPage()),
+                    );
+                  },
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(Icons.info),
+                      SizedBox(width: 10),
+                      Text(
+                        'Tentang Kami',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Spacer(),
+                      Icon(Icons.arrow_forward_ios),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextButton(
+                  onPressed: () {
+                    // Navigate to the Log Out page
+                  },
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(Icons.logout, color: Colors.red),
+                      SizedBox(width: 10),
+                      Text('Log Out',
+                          style: TextStyle(fontSize: 18, color: Colors.red)),
+                      Spacer(),
+                      Icon(Icons.arrow_forward_ios, color: Colors.red),
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
