07924365ebac9c9536630b656d57dfc48a7e0c04
Andi Dwi Prastyo <2141720046@student.polinema.ac.id>
2023-11-19 21:59:35 +0700

    feat:Add Identity Card

--- src/lib/ui/providers/identity_card_provider.dart
+++ src/lib/ui/providers/identity_card_provider.dart
@@ -1,19 +1,39 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../domain/models/identity_card_model.dart';
+import '../../domain/repositories/identity_card_repository.dart';
 
 part 'identity_card_provider.g.dart';
 
 @riverpod
 class IdentityCardNotifier extends _$IdentityCardNotifier {
   @override
-  Stream<List<IdentityCardModel>> build() => Stream.value([]);
+  List<IdentityCardModel> build() => [];
+
+  IdentityCardRepository get _repository =>
+      ref.read(identityCardRepositoryProvider);
 
   Future<void> save(IdentityCardModel card) async {
-    // TODO: implement save
+    if (card.id == null) {
+      await _repository.create(card);
+
+      state = [...state, card];
+    } else {
+      await _repository.update(card);
+
+      state = [
+        for (final item in state)
+          if (item.id == card.id) card else item
+      ];
+    }
   }
 
   Future<void> delete(IdentityCardModel card) async {
-    // TODO: implement delete
+    await _repository.delete(card);
+
+    state = [
+      for (final item in state)
+        if (item.id != card.id) item
+    ];
   }
 }
