1f7327d13fcaf440ae68cf1bfe45ebcf28f50428
BimaBayuUWUUU <bimabayus7994@gmail.com>
2023-11-19 21:47:06 +0700

    add_id_card_repo

--- src/lib/data/repositories/identity_card_repository_impl.dart
+++ src/lib/data/repositories/identity_card_repository_impl.dart
@@ -1,5 +1,29 @@
+import '../../domain/models/identity_card_model.dart';
 import '../../domain/repositories/identity_card_repository.dart';
 
 class IdentityCardRepositoryImpl implements IdentityCardRepository {
-  // TODO: Implement
+  @override
+  Future<void> create(IdentityCardModel card) async {
+    return;
+  }
+
+  @override
+  Future<void> delete(IdentityCardModel card) async {
+    return;
+  }
+
+  @override
+  Future<IdentityCardModel?> get(String id) async {
+    return null;
+  }
+
+  @override
+  Future<void> update(IdentityCardModel card) async {
+    return;
+  }
+
+  @override
+  Future<List<IdentityCardModel>> getAll() async {
+    return [];
+  }
 }
