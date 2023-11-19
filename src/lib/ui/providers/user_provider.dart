45b848a06063fa6742d464e69b32ed68d51e3a89
Andi Dwi Prastyo <2141720046@student.polinema.ac.id>
2023-11-19 22:00:20 +0700

    feat:Add User Provider

--- src/lib/ui/providers/user_provider.dart
+++ src/lib/ui/providers/user_provider.dart
@@ -1,19 +1,39 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-import '../../domain/models/user_model.dart';
+import '../../domain/models/identity_card_model.dart';
+import '../../domain/repositories/identity_card_repository.dart';
 
-part 'user_provider.g.dart';
+part 'identity_card_provider.g.dart';
 
 @riverpod
-class UserNotifier extends _$UserNotifier {
+class IdentityCardNotifier extends _$IdentityCardNotifier {
   @override
-  Stream<UserModel?> build() => Stream.value(UserModel());
+  List<IdentityCardModel> build() => [];
 
-  Future<void> register() async {
-    // TODO: implement register
+  IdentityCardRepository get _repository =>
+      ref.read(identityCardRepositoryProvider);
+
+  Future<void> save(IdentityCardModel card) async {
+    if (card.id == null) {
+      await _repository.create(card);
+
+      state = [...state, card];
+    } else {
+      await _repository.update(card);
+
+      state = [
+        for (final item in state)
+          if (item.id == card.id) card else item
+      ];
+    }
   }
 
-  Future<void> login() async {
-    // TODO: implement login
+  Future<void> delete(IdentityCardModel card) async {
+    await _repository.delete(card);
+
+    state = [
+      for (final item in state)
+        if (item.id != card.id) item
+    ];
   }
 }
