58ed03107e3922649d4ebdb04a4f29083c750245
AdamPoi <isadamrr@gmail.com>
2023-11-19 21:37:36 +0700

    feat: add identity card repository

--- src/lib/domain/repositories/identity_card_repository.dart
+++ src/lib/domain/repositories/identity_card_repository.dart
@@ -1,3 +1,19 @@
-class IdentityCardRepository {
-  // TODO: Implement
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../data/repositories/identity_card_repository_impl.dart';
+import '../models/identity_card_model.dart';
+
+part 'identity_card_repository.g.dart';
+
+abstract class IdentityCardRepository {
+  Future<void> create(IdentityCardModel card);
+  Future<void> update(IdentityCardModel card);
+  Future<void> delete(IdentityCardModel card);
+  Future<IdentityCardModel?> get(String id);
+  Future<List<IdentityCardModel>> getAll();
+}
+
+@riverpod
+IdentityCardRepository identityCardRepository(IdentityCardRepositoryRef ref) {
+  return IdentityCardRepositoryImpl();
 }
---
+++ src/lib/domain/repositories/identity_card_repository.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'identity_card_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$identityCardRepositoryHash() =>
+    r'715bc49fb3362ab98621d0e809bf0d783dad0eae';
+
+/// See also [identityCardRepository].
+@ProviderFor(identityCardRepository)
+final identityCardRepositoryProvider =
+    AutoDisposeProvider<IdentityCardRepository>.internal(
+  identityCardRepository,
+  name: r'identityCardRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$identityCardRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef IdentityCardRepositoryRef
+    = AutoDisposeProviderRef<IdentityCardRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
