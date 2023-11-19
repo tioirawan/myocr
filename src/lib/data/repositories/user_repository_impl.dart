20008ce6d4cba1e9d17db51111cdb28505b5d392
BimaBayuUWUUU <bimabayus7994@gmail.com>
2023-11-19 21:47:18 +0700

    add_user_repo

--- src/lib/data/repositories/user_repository_impl.dart
+++ src/lib/data/repositories/user_repository_impl.dart
@@ -1,5 +1,31 @@
+import '../../domain/models/user_model.dart';
 import '../../domain/repositories/user_repository.dart';
 
 class UserRepositoryImpl implements UserRepository {
-  // TODO: Implement
+  @override
+  Future<UserModel?> register(
+      String username,
+      String email,
+      String password,
+      ) async {
+    return UserModel();
+  }
+
+  @override
+  Future<UserModel?> login(
+      String username,
+      String password,
+      ) async {
+    return UserModel();
+  }
+
+  @override
+  Future<bool> logout() async {
+    return true;
+  }
+
+  @override
+  Future<UserModel?> currentUser() async {
+    return UserModel();
+  }
 }
