fc32dc3426938d63c869df29055031144deafa49
AdamPoi <isadamrr@gmail.com>
2023-11-19 21:37:17 +0700

    feat: add user repository

--- src/lib/domain/repositories/user_repository.dart
+++ src/lib/domain/repositories/user_repository.dart
@@ -1,3 +1,18 @@
-class UserRepository {
-  // TODO: Implement
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../data/repositories/user_repository_impl.dart';
+import '../models/user_model.dart';
+
+part 'user_repository.g.dart';
+
+abstract class UserRepository {
+  Future<UserModel?> register(String username, String email, String password);
+  Future<UserModel?> login(String username, String password);
+  Future<UserModel?> currentUser();
+  Future<bool> logout();
+}
+
+@riverpod
+UserRepository userRepository(UserRepositoryRef ref) {
+  return UserRepositoryImpl();
 }
