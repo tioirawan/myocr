ce67aa6d5e5b8ea13298357e8b4cf2ff024410fa
Tio Irawan <sattanationmail@gmail.com>
2023-11-27 09:45:20 +0700

    feat: add firebase dependencies and configuration files

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'firebase_options.dart';
 import 'ui/pages/about_page.dart';
 import 'ui/pages/auth/login_page.dart';
 import 'ui/pages/auth/register_page.dart';
@@ -14,7 +16,13 @@ import 'ui/pages/scan/success_page.dart';
 import 'ui/pages/splash_screen_page.dart';
 import 'ui/themes/color_schemes.g.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     const ProviderScope(child: MainApp()),
   );
