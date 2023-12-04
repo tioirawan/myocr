7879a29f16925f4d68c88b15ad901c0fe8f97c5d
AdamPoi <isadamrr@gmail.com>
2023-12-04 09:54:06 +0700

    test: implement idcard firestore

--- src/lib/data/repositories/identity_card_repository_impl.dart
+++ src/lib/data/repositories/identity_card_repository_impl.dart
@@ -1,29 +1,56 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import '../../domain/models/identity_card_model.dart';
 import '../../domain/repositories/identity_card_repository.dart';
 
 class IdentityCardRepositoryImpl implements IdentityCardRepository {
+  final db = FirebaseFirestore.instance;
+
   @override
   Future<void> create(IdentityCardModel card) async {
-    return;
+    await db.collection('identity_cards').add(card.toJson()).then(
+          (doc) => print("Add Document with id ${doc.id}"),
+          onError: (e) => print("Error adding document $e"),
+        );
   }
 
   @override
   Future<void> delete(IdentityCardModel card) async {
-    return;
+    await db.collection('identity_cards').doc(card.id).delete().then(
+          (doc) => print("Document deleted"),
+          onError: (e) => print("Error updating document $e"),
+        );
   }
 
   @override
   Future<IdentityCardModel?> get(String id) async {
+    await db.collection('identity_cards').doc(id).get().then(
+      (DocumentSnapshot doc) {
+        return doc.data() as Future<List<IdentityCardModel>>;
+      },
+      onError: (e) => print("Error getting document: $e"),
+    );
     return null;
   }
 
   @override
   Future<void> update(IdentityCardModel card) async {
-    return;
+    await db
+        .collection('identity_cards')
+        .doc(card.id)
+        .update(card.toJson())
+        .then((value) => print("DocumentSnapshot successfully updated!"),
+            onError: (e) => print("Error updating document $e"));
   }
 
   @override
   Future<List<IdentityCardModel>> getAll() async {
+    await db.collection('identity_cards').get().then(
+      (QuerySnapshot docs) {
+        return docs as Future<List<IdentityCardModel>>;
+      },
+      onError: (e) => print("Error getting document: $e"),
+    );
     return [];
   }
 }
