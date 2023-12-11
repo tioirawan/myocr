b905b5933e679f691fd30d74a96c1a8869d54380
Tio Irawan <sattanationmail@gmail.com>
2023-12-11 14:59:38 +0700

    W16: Fix error incorect parent data

--- src/lib/ui/widgets/identity_card_tile.dart
+++ src/lib/ui/widgets/identity_card_tile.dart
@@ -25,112 +25,110 @@ class _IdentityCardTileState extends ConsumerState<IdentityCardTile> {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        margin: const EdgeInsets.all(8),
-        decoration: BoxDecoration(
+    return Container(
+      margin: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.1),
+            spreadRadius: 2,
+            blurRadius: 20,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Material(
+        borderRadius: BorderRadius.circular(8),
+        child: InkWell(
           borderRadius: BorderRadius.circular(8),
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.1),
-              spreadRadius: 2,
-              blurRadius: 20,
-              offset: const Offset(0, 4),
+          onTap: widget.onTap,
+          child: ListTile(
+            contentPadding:
+                const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            leading: ClipRRect(
+              borderRadius: BorderRadius.circular(4),
+              child: widget.document.cardImageUrl != null
+                  ? Image.network(
+                      widget.document.cardImageUrl!,
+                      fit: BoxFit.contain,
+                    )
+                  : Image.asset(
+                      'assets/images/dummyktp.png',
+                      fit: BoxFit.contain,
+                    ),
             ),
-          ],
-        ),
-        child: Material(
-          borderRadius: BorderRadius.circular(8),
-          child: InkWell(
-            borderRadius: BorderRadius.circular(8),
-            onTap: widget.onTap,
-            child: ListTile(
-              contentPadding:
-                  const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-              leading: ClipRRect(
-                borderRadius: BorderRadius.circular(4),
-                child: widget.document.cardImageUrl != null
-                    ? Image.network(
-                        widget.document.cardImageUrl!,
-                        fit: BoxFit.contain,
-                      )
-                    : Image.asset(
-                        'assets/images/dummyktp.png',
-                        fit: BoxFit.contain,
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.document.name ?? '-',
+                        style: const TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
                       ),
-              ),
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          widget.document.name ?? '-',
-                          style: const TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          '${widget.document.nik} - ${widget.document.gender}',
-                          style: const TextStyle(fontSize: 10),
-                        ),
-                        const SizedBox(height: 5),
-                      ],
+                      Text(
+                        '${widget.document.nik} - ${widget.document.gender}',
+                        style: const TextStyle(fontSize: 10),
+                      ),
+                      const SizedBox(height: 5),
+                    ],
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: _download,
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: lightColorScheme.primary,
+                    minimumSize: const Size(20, 25),
+                    padding: const EdgeInsets.symmetric(horizontal: 14),
+                  ),
+                  child: _isDownloading
+                      ? const SizedBox(
+                          width: 10,
+                          height: 10,
+                          child: CircularProgressIndicator(
+                            color: Colors.white,
+                            strokeWidth: 2,
+                          ),
+                        )
+                      : const Text('Unduh', style: TextStyle(fontSize: 10)),
+                ),
+              ],
+            ),
+            subtitle: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  width: 60,
+                  height: 18,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(
+                      color: widget.isValid ? Colors.green : Colors.red,
+                      width: 1,
                     ),
                   ),
-                  ElevatedButton(
-                    onPressed: _download,
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.white,
-                      backgroundColor: lightColorScheme.primary,
-                      minimumSize: const Size(20, 25),
-                      padding: const EdgeInsets.symmetric(horizontal: 14),
-                    ),
-                    child: _isDownloading
-                        ? const SizedBox(
-                            width: 10,
-                            height: 10,
-                            child: CircularProgressIndicator(
-                              color: Colors.white,
-                              strokeWidth: 2,
-                            ),
-                          )
-                        : const Text('Unduh', style: TextStyle(fontSize: 10)),
-                  ),
-                ],
-              ),
-              subtitle: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    width: 60,
-                    height: 18,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      border: Border.all(
+                  child: Center(
+                    child: Text(
+                      widget.isValid ? 'Valid' : 'Tidak Valid',
+                      style: TextStyle(
                         color: widget.isValid ? Colors.green : Colors.red,
-                        width: 1,
-                      ),
-                    ),
-                    child: Center(
-                      child: Text(
-                        widget.isValid ? 'Valid' : 'Tidak Valid',
-                        style: TextStyle(
-                          color: widget.isValid ? Colors.green : Colors.red,
-                          fontSize: 8,
-                          fontWeight: FontWeight.bold,
-                        ),
+                        fontSize: 8,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
-                  Text(
-                    widget.document.createdAt?.toString() ?? '-',
-                    style: const TextStyle(fontSize: 8),
-                  ),
-                ],
-              ),
+                ),
+                Text(
+                  widget.document.createdAt?.toString() ?? '-',
+                  style: const TextStyle(fontSize: 8),
+                ),
+              ],
             ),
           ),
         ),
